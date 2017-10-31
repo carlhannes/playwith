59fbc1fbcb24e6e6aa091eb27735a1a77dbbce7d
Hannes Lindquist <hannes.lindquist@gmail.com>
2017-10-31 07:05:31 +0100

    Editor added

--- src/editor.js
+++ src/editor.js
@@ -1,11 +1,24 @@
 const { ace } = window;
 
+const JavaScriptMode = ace.require('ace/mode/javascript').Mode;
+
 export default function createEditor(id) {
   function myEditor(value) {
     if (typeof value !== 'undefined') {
-      myEditor.setValue(value);
+      myEditor.ace.setValue(value, -1);
     }
 
-    return myEditor;
+    return myEditor.ace.getValue();
   }
+
+  myEditor.ace = ace.edit(id);
+  myEditor.ace.$blockScrolling = Infinity;
+  myEditor.ace.session.setMode(new JavaScriptMode());
+  myEditor.ace.setOptions({
+    fontSize: '14px',
+    tabSize: 2,
+    useSoftTabs: true
+  });
+
+  return myEditor;
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import 'ace-builds/src-noconflict/ace';
 import 'ace-builds/src-noconflict/mode-javascript';
+import lq from 'litequery';
 
 import './style.less';
 
+import createEditor from './editor';
+
+let leftEditor;
+// let rightEditor;
 
 console.log('Hey! Im playwith');
+
+lq(document).on('DOMContentLoaded', () => {
+  leftEditor = createEditor('editor-left');
+
+  lq('#editor-left').on('keyup', () => {
+    console.log(leftEditor());
+  });
+});
